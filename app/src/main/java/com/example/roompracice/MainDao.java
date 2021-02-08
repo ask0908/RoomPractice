369f46032637c7c9e6fee8f64add17c4a22a2875
ask0908 <ask0908@naver.com>
2021-02-09 01:15:45 +0900

    공부 내용 추가

--- app/src/main/java/com/example/roompracice/MainDao.java
+++ app/src/main/java/com/example/roompracice/MainDao.java
@@ -9,14 +9,19 @@ import java.util.List;
 
 import static androidx.room.OnConflictStrategy.REPLACE;
 
+/* Room에선 직접 쿼리로 접근하는 대신 DAO(Data Access Object)를 써서 DB에 접근해야 한다
+* DAO는 인터페이스 or 추상클래스로 구현돼야 한다. 기존 쿼리의 큰 문제는 컴파일 시 쿼리상의 오류를 발견하지 못하는 것이었는데
+* Room DAO에선 작성된 쿼리의 오류를 컴파일 시에 발견해 미리 오류를 수정할 수 있다. 또한 Entity 자동완성을 지원해서 쿼리 작성 시 오탈자가 날 확률도 줄였다 */
 @Dao
 public interface MainDao
 {
     // INSERT문
+    // @Insert : 데이터를 입력할 때 사용, 단일 객체부터 배열 형태 데이터까지 입력 가능, 입력 시 옵션을 지정해 충돌나면 어떻게 대처할지도 설정 가능
     @Insert(onConflict = REPLACE)
     void insert(MainData mainData);
 
     // DELETE문
+    // @Delete : 전달받은 인자의 PK값에 매칭되는 entity를 찾아 삭제한다
     @Delete
     void delete(MainData mainData);
 
@@ -25,6 +30,8 @@ public interface MainDao
     void reset(List<MainData> mainData);
 
     // UPDATE문
+    // @Query : 주로 데이터 선택 시 사용, DAO 클래스에서 가장 핵심적인 부분, 데이터를 읽고 쓸 수 있게 한다
+    // 컴파일 시 쿼리검사가 이뤄지기 때문에 런타임 오류를 최소화할 수 있다
     @Query("UPDATE table_name SET text = :sText WHERE ID = :sID")
     void update(int sID, String sText);
 
--- app/src/main/java/com/example/roompracice/MainData.java
+++ app/src/main/java/com/example/roompracice/MainData.java
@@ -7,10 +7,15 @@ import androidx.room.PrimaryKey;
 import java.io.Serializable;
 
 // Define table name
+/* @Entity는 DB의 테이블 역할을 한다. 아래처럼 테이블명을 사용자가 정할 수 있다. 테이블명의 초기값은 클래스명이며 이 때 대소문자는 구별되지 않는다
+* @PrimaryKey로 PK값을 지정할 수 있고 중복 시 오류가 발생한다. autoGenerate=true로 자동으로 Key 값을 생성할 수 있다
+* @ColumnInfo로 컬럼명을 지정한다 */
 @Entity(tableName = "table_name")
 public class MainData implements Serializable
 {
     // id 컬럼 생성
+    // Room Entity는 반드시 1개 이상의 PK를 갖고 있어야 한다. 이 때 PK값은 별개의 필드값으로 만들고 autoGenerate=true로 설정하는 게 바람직하다
+    // autoGenerate=true : 유니크한 id값을 자동으로 생성한다
     @PrimaryKey(autoGenerate = true)
     private int id;
 
@@ -19,7 +24,6 @@ public class MainData implements Serializable
     private String text;
 
     // 게터세터 생성
-
     public int getId()
     {
         return id;
